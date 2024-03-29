35424689f6e12b350a5919f886b2c47e894b6cbe
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-03-29 07:26:08 +0700

    Add files via upload

---
+++ GOLIKE.php
@@ -0,0 +1,155 @@
+<?php
+$ngay = date("d");
+$thang = date("m");
+$nam = date("Y");
+
+
+$red = "\033[1;31m";
+
+$green = "\033[1;32m";
+
+$yellow = "\033[1;33m";
+
+$blud = "\033[1;34m";
+
+$res = "\033[1;35m";
+
+$nau = "\033[1;36m";
+
+$trang = "\033[1;37m";
+
+$cam = "\e[38;5;208m";
+
+system('clear');
+echo $yellow."~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+
+echo $green."             ♨️Tool 🌋LE ANH VU🌋    \n         🌈:🔥LEANHVU🔥 \n           🌏 $ngay/$thang/$nam 🌏\n\n";
+echo $yellow."~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+
+
+usleep(1);
+
+
+
+echo " like \n\n";
+
+echo "💠]>$blud Nhập $res [1] $cam $red Lavvip1like.php   $cam 》$nau -$yellow Lavvip1like.php\n";
+echo "💠]>$blud Nhập $res [2] $cam $red Lavvip2like.php   $cam 》$nau -$yellow Lavvip2like.php\n";
+echo "💠]>$blud Nhập $res [3] $cam $red Lavvip3like.php   $cam 》$nau -$yellow Lavvip3like.php\n";
+echo "💠]>$blud Nhập $res [4] $cam $red Lavvip4like.php   $cam 》$nau -$yellow Lavvip4like.php\n";
+echo "💠]>$blud Nhập $res [5] $cam $red Lavvip5like.php   $cam 》$nau -$yellow Lavvip5like.php\n";
+echo "💠]>$blud Nhập $res [6] $cam $red Lavvip6like.php   $cam 》$nau -$yellow Lavvip6like.php\n";
+echo "💠]>$blud Nhập $res [7] $cam $red Lavvip7like.php   $cam 》$nau -$yellow Lavvip7like.php\n";
+echo "💠]>$blud Nhập $res [8] $cam $red Lavvip8like.php   $cam 》$nau -$yellow Lavvip8like.php\n";
+echo "💠]>$blud Nhập $res [9] $cam $red Lavvip9like.php   $cam 》$nau -$yellow Lavvip9like.php\n";
+echo "💠]>$blud Nhập $res [10] $cam $red dothihong1992like.php   $cam 》$nau -$yellow dothihong1992like.php\n";
+echo "💠]>$blud Nhập $res [11] $cam $red hothithuy57like.php   $cam 》$nau -$yellow hothithuy57like.php\n";
+echo "💠]>$blud Nhập $res [12] $cam $red leanhvu1985like.php   $cam 》$nau -$yellow leanhvu1985like.php\n";
+echo "💠]>$blud Nhập $res [13] $cam $red vuanhle85_like.php   $cam 》$nau -$yellow vuanhle85_like.php\n";
+
+
+
+
+
+
+
+
+
+
+
+
+echo "UPDATE      \n";
+
+echo "💠]>$blud Nhập $res [0] $cam $red UPDATE $cam 》$nau -$yellow [][][][][]\n";
+
+
+
+echo "$red |======================================================|\n";
+while(true){
+echo "$trang ♨️$red>>[$nau leanhvu $red]$nau=》";
+
+
+
+
+
+
+$chon=trim(fgets(STDIN));
+
+if ($chon == '1') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip1like.php'));
+
+} 
+if ($chon == '2') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip2like.php'));
+
+} 
+if ($chon == '3') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip3like.php'));
+
+} 
+if ($chon == '4') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip4like.php'));
+
+} 
+if ($chon == '5') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/lavvip5like.php'));
+
+} 
+if ($chon == '6') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip6like.php'));
+
+} 
+if ($chon == '7') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip7like.php'));
+
+} 
+if ($chon == '8') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip8gatewaylike.php'));
+
+} 
+if ($chon == '9') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/Lavvip9gateway.php'));
+
+} 
+if ($chon == '10') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/dothihong1992like.php'));
+
+} 
+if ($chon == '11') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/hothithuy57like.php'));
+
+} 
+if ($chon == '12') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/leanhvu1985like.php'));
+
+} 
+if ($chon == '13') {
+echo "ĐANG KẾT NỐI\n";
+eval(file_get_contents('https://raw.githubusercontent.com/Lav141285/PHP-golike-tiktok/main/vuanhle85_like.php'));
+
+} 
+
+
+
+
+
+
+
+
+
+else if($chon == '0') 
+echo "CHƯA UPDATE\n";
+}
+

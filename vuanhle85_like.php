5922c930eac768f7f5347a1735508bd19264e899
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-03-29 20:56:05 +0700

    Update vuanhle85_like.php

--- vuanhle85_like.php
+++ vuanhle85_like.php
@@ -133,7 +133,7 @@ $html="htmlspecialchars_decode";
 $a1="Host:gateway.golike.net";
 $a2="user-agent: Mozilla/5.0 (Linux; Android 12; SM-A025F Build/SP1A.210812.016;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
-$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTA0NDIyODksImV4cCI6MTc0MTk3ODI4OSwibmJmIjoxNzEwNDQyMjg5LCJqdGkiOiIwOUNKbkxOU2xiOE1iTHdIIiwic3ViIjoyMzE2OTIwLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.lJ4rdarFwJ06dWYm0QcROijrvuk_sZq_-hl5Vp3osFE    ";/*[Tool Tao Cấm Buôn Bán ]*/
+$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTE3MjA0MjcsImV4cCI6MTc0MzI1NjQyNywibmJmIjoxNzExNzIwNDI3LCJqdGkiOiJmSUFpalFwNXkwNm5qNEQ2Iiwic3ViIjoyMzE2OTIwLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.MC2XJG3wF6Eji8N1papYDe4ypj3IRdofP0G9esbE6n0     ";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
 $a4="origin: https://app.golike.net";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/

7903eef94cc98023abf4f7a48357edfcc9c9ec08
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-03-29 22:08:03 +0700

    Update followlike.php

--- followlike.php
+++ followlike.php
@@ -133,7 +133,7 @@ $html="htmlspecialchars_decode";
 $a1="Host:gateway.golike.net";
 $a2="user-agent: Mozilla/5.0 (Linux; Android 12; SM-A025F Build/SP1A.210812.016;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
-$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTE3MDQ3MzQsImV4cCI6MTc0MzI0MDczNCwibmJmIjoxNzExNzA0NzM0LCJqdGkiOiJwdm96YkpMcmt6VDR2NVNJIiwic3ViIjoyNTA0OTgzLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.e1oTxSO6G1BUfOItdIrc6KYllZFUFAZWzdHoKkBwIAc     ";/*[Tool Tao Cấm Buôn Bán ]*/
+$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTE3MjQ4MjMsImV4cCI6MTc0MzI2MDgyMywibmJmIjoxNzExNzI0ODIzLCJqdGkiOiJFdXJBMTNmYUk4MWIyR09DIiwic3ViIjoyNjE4MTUyLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.RDJo8p4azWuPlqt0kiAEVh2bUbyqjvEOtYpnHYDVTdo   ";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
 $a4="origin: https://app.golike.net";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/

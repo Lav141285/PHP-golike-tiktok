abd5f7244f8385b7b5def4a5c4ab6a344fdcf48a
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-03-29 21:16:50 +0700

    Update leanhvu1985like.php

--- leanhvu1985like.php
+++ leanhvu1985like.php
@@ -133,7 +133,7 @@ $html="htmlspecialchars_decode";
 $a1="Host:gateway.golike.net";
 $a2="user-agent: Mozilla/5.0 (Linux; Android 12; SM-A025F Build/SP1A.210812.016;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
-$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTE3MDgyMjcsImV4cCI6MTc0MzI0NDIyNywibmJmIjoxNzExNzA4MjI3LCJqdGkiOiJRQVF5b0lRanpCd0dST21YIiwic3ViIjoyNDg2NDYzLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.2lEm4v0JqjAf0ddzbb5JTKGozLxbBvUPw3EAhIn2KDo    ";/*[Tool Tao Cấm Buôn Bán ]*/
+$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTE3MjE3NTQsImV4cCI6MTc0MzI1Nzc1NCwibmJmIjoxNzExNzIxNzU0LCJqdGkiOiIyQVBWVWNJNFpYcDZoc1BzIiwic3ViIjoyNDg2NDYzLCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.b098SF3dJabi7CCbzLtccqZ3B6QZT5HnnHtANwYrA94     ";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
 $a4="origin: https://app.golike.net";/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/

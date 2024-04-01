43541c8407345ab6d01657336891e5fdd3028a87
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-01 16:02:35 +0700

    Update followlike.php

--- followlike.php
+++ followlike.php
@@ -242,7 +242,7 @@ system("termux-open $link");/*[Tool Tao Cấm Buôn Bán ]*/
 
 
 for ($time = 15; $time > 0; $time--) {
-    echo $yellow."Vui lòng đợi $time để thực hiện...... \r"; sleep(0);
+    echo $yellow."Vui lòng đợi $time để thực hiện...... \r"; sleep(1);
 }
 
 

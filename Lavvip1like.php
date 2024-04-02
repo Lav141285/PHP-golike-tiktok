3260d8dbd4b2a9ec37f40f1971c0a83db314be28
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-02 16:34:09 +0700

    update Lavvip1like.php

--- Lavvip1like.php
+++ Lavvip1like.php
@@ -221,7 +221,7 @@ if($st=="400"){echo "Đang Load Lại Job            \r";sleep(0);continue;}else
 /*[Tools Nguyễn Văn Phúc ]*/
 ("termux-open $link");/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
-for($time=$9999;$time>0;$time--){
+for($time=9999;$time>0;$time--){
 echo $yellow."Vui lòng đợi $time để thực hiện...... \r";sleep(0);}/*[Tool Tao Cấm Buôn Bán ]*/
 
 

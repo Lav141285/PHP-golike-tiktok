307d6a3400210545531d8d7aa6f752ecfb221ac8
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-02 16:27:14 +0700

    update Lavvip1like.php

--- Lavvip1like.php
+++ Lavvip1like.php
@@ -254,7 +254,7 @@ $se = $nhan['success'];
 /*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
 
-if($se == "1"){/*[Tool Tao Cấm Buôn Bán ]*/
+if($se == "10"){/*[Tool Tao Cấm Buôn Bán ]*/
 /*[Tools Nguyễn Văn Phúc ]*/
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 $ngay=date("H:i");$noe++;/*[Tool Tao Cấm Buôn Bán ]*/

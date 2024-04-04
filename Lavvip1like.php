e8763548937e71901ac4c7d18113dbfe178577fb
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-04 20:05:25 +0700

    update Lavvip1like.php

--- Lavvip1like.php
+++ Lavvip1like.php
@@ -187,7 +187,7 @@ $loi = json_decode($loi,true);
 $mes = $loi['message'];
 if($mes == "Tôi không muốn làm Job này"){
 $p = $red."Thành công           \r";
-cc($p);}else{echo $yellow."Tôi không muốn làm Job này\n";}
+cc($p);}else{echo $yellow."Tôi không muốn làm Job này   \n";}
 continue;}else{}
 
 if($st=="429"){echo "Bạn vừa thực hiện thao tác quá nhanh,vui lòng thử lại sau 10s   \r";sleep(1);continue;}else{echo "Đang Làm job $ty | $uid          \r";}

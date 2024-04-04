be7ca04c6801e45aa04c56e1f389963eb9b8fc80
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-04 19:34:21 +0700

    update Lavvip1like.php

--- Lavvip1like.php
+++ Lavvip1like.php
@@ -1,5 +1,4 @@
 
-
 $red="\033[1;31m";
 
 $green="\033[1;32m";
@@ -186,14 +185,12 @@ $tsm3 = array($a1,$a2,$a3,$a4,$a5,"content-length: ".strlen($data),"content-type
 $loi = post("https://gateway.golike.net/api/advertising/publishers/tiktok/skip-jobs",$tsm3,$data);
 $loi = json_decode($loi,true);
 $mes = $loi['message'];
-if($mes == "Bỏ qua thành công !"){echo "Đang Load Lại Job            \r";sleep(1);continue;}
-
-
+if($mes == "Bỏ qua thành công !"){
 $p = $red."Báo Cáo lỗi Thành công           \r";
 cc($p);}else{echo $yellow."Đã Xảy Ra Lỗi Báo Ad Nguyễn Văn Phúc nhé  \n => telegram : @nvp2912\n";}
 continue;}else{}
 
-if($st=="400"){echo "Đang Load Lại Job            \r";sleep(1);continue;}else{echo "Đang Làm job $ty | $uid          \r";}
+if(($mes == "Bỏ qua thành công !"){echo "Đang Load Lại Job            \r";sleep(1);continue;}else{echo "Đang Làm job $ty | $uid          \r";}
 
 ("termux-open-url $link");
 
@@ -239,7 +236,7 @@ $tsm3 = array($a1,$a2,$a3,$a4,$a5,"content-length: ".strlen($data),"content-type
 $loi = post("https://gateway.golike.net/api/advertising/publishers/tiktok/skip-jobs",$tsm3,$data);
 $loi = json_decode($loi,true);
 $mes = $loi['message'];
-if($mes == "Bỏ qua thành công !"){echo "Đang Load Lại Job            \r";sleep(1);continue;}
+if($mes == "Bỏ qua thành công !"){
 $p = $red."Báo Cáo lỗi Thành công           \r";
 cc($p);}else{echo $yellow."Đã Xảy Ra Lỗi Báo Ad Nguyễn Văn Phúc nhé \n => telegram : @nvp2912\n";}
 

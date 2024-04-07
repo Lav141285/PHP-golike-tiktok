260672f93c15f0a8ac1d13a1878f002ef4493c4a
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-07 20:39:59 +0700

    update Lavvip1like.php

--- Lavvip1like.php
+++ Lavvip1like.php
@@ -211,7 +211,7 @@ continue;}else{}
  
  
  if ($st >= "400") {
-    echo "Đang Load Lại Job            \r"; sleep(20); continue;
+    echo "Đang Load Lại Job            \r"; sleep(15); continue;
 } else {
     echo "Đang Làm job $ty | $uid          \r";
 }

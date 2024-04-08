cafc5223f8207b1337e4982d13ee39534d07c6d9
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-08 13:40:03 +0700

    update hothithuy57like.php

--- hothithuy57like.php
+++ hothithuy57like.php
@@ -199,7 +199,7 @@ continue;}else{}
  
  
  if ($st >= "400") {
-    echo "Đang Load Lại Job            \r"; sleep(10); continue;
+    echo "Đang Load Lại Job            \r"; sleep(0); continue;
 } else {
     echo "Đang Làm job $ty | $uid          \r";
 }

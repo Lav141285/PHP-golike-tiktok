bf2fe6bda19d935518f5b8909b75e2218db93e0d
Lav141285 <153383033+Lav141285@users.noreply.github.com>
2024-04-10 01:08:04 +0700

    update Lavvip3like.php

--- Lavvip3like.php
+++ Lavvip3like.php
@@ -1,110 +1,122 @@
 
-$red="\033[1;31m";
-
-$green="\033[1;32m";
-
-$yellow="\033[1;33m";
-
-$blud="\033[1;34m";
-
-$res="\033[1;35m";
-
-$nau="\033[1;36m";
-
-$trang="\033[1;37m";
-
-$cam= "\e[38;5;208m";
-
-$loijob=[];
-
-$loitk=[];
-
+$ngay = date("d");
+$thang = date("m");
+$nam = date("Y");
+ 
+ 
+$red = "\033[1;31m";
+ 
+$green = "\033[1;32m";
+ 
+$yellow = "\033[1;33m";
+ 
+$blud = "\033[1;34m";
+ 
+$res = "\033[1;35m";
+ 
+$nau = "\033[1;36m";
+ 
+$trang = "\033[1;37m";
+ 
+$cam = "\e[38;5;208m";
+ 
+$loijob = [];
+ 
+$loitk = [];
+ 
 error_reporting(0);
 system('clear');
-
-
-
-function GET($host,$tsm){
-  $mr = curl_init();
-
-  curl_setopt_array($mr, array(
-  CURLOPT_PORT => "443",
-
-  CURLOPT_URL => "$host",
-
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_SSL_VERIFYPEER => false,
-
-  CURLOPT_CUSTOMREQUEST => "GET",
-
-  CURLOPT_HTTPHEADER => $tsm,
-
-
-  CURLOPT_FOLLOWLOCATION => true,));
-
-  $mr2 = curl_exec($mr); curl_close($mr);
-  return $mr2;}
-
-  
-  function HEAD($host,$tsm,$data){
-  $mr = curl_init();
-
-  curl_setopt_array($mr, array(
-
-  CURLOPT_PORT => "443",
-  CURLOPT_URL => "$host",
-
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_SSL_VERIFYPEER => false,
-
-  CURLOPT_CUSTOMREQUEST => "HEAD",
-  CURLOPT_POSTFIELDS => $data,
-  CURLOPT_HTTPHEADER => $tsm,
-
-  CURLOPT_HEADER => true));
-  $mr2 = curl_exec($mr); curl_close($mr);
- return $mr2;}
-
-
-function POST($host,$tsm,$data){
-  $mr = curl_init();
-
-  curl_setopt_array($mr, array(
-
-  CURLOPT_PORT => "443",
-  CURLOPT_URL => "$host",
-  CURLOPT_RETURNTRANSFER => true,
-
-
-  CURLOPT_SSL_VERIFYPEER => false,
-
-  CURLOPT_CUSTOMREQUEST => "POST",
-
-  CURLOPT_POSTFIELDS => $data,
-  CURLOPT_HTTPHEADER => $tsm,
-
-  //CURLOPT_HEADER => true
-));
-  $mr2 = curl_exec($mr); curl_close($mr);
-
- return $mr2;}
-
-
-
- function cc($vanban){$str = strlen($vanban);
- for($i=0;$i<=$str;$i++){echo $vanban[$i]; usleep(1000);}
-return 1;}
-
-
-
-
-
-$a1="Host: dev.golike.net";
-$a2="user-agent: Mozilla/5.0 (Linux; Android 12; SM-A025F Build/SP1A.210812.016;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36";
-
-$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTI2ODM2NjMsImV4cCI6MTc0NDIxOTY2MywibmJmIjoxNzEyNjgzNjYzLCJqdGkiOiJNQjZUcnA4YjZMWXJHTlhiIiwic3ViIjoyNTI3NjQ5LCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.-0exk2yXYgULWBnNjgQZyJOouAP5wilZo_bqj_xK8nc     ";/*[Tool Tao Cấm Buôn Bán ]*/
-$a4 = "origin: https://app.golike.net";
  
+ 
+ 
+function GET($host, $tsm) {
+    $mr = curl_init();
+ 
+    curl_setopt_array($mr, array(
+        CURLOPT_PORT => "443",
+ 
+        CURLOPT_URL => "$host",
+ 
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+ 
+        CURLOPT_CUSTOMREQUEST => "GET",
+ 
+        CURLOPT_HTTPHEADER => $tsm,
+ 
+ 
+        CURLOPT_FOLLOWLOCATION => true,));
+ 
+    $mr2 = curl_exec($mr); curl_close($mr);
+    return $mr2;
+}
+ 
+ 
+function HEAD($host, $tsm, $data) {
+    $mr = curl_init();
+ 
+    curl_setopt_array($mr, array(
+ 
+        CURLOPT_PORT => "443",
+        CURLOPT_URL => "$host",
+ 
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+ 
+        CURLOPT_CUSTOMREQUEST => "HEAD",
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => $tsm,
+ 
+        CURLOPT_HEADER => true));
+    $mr2 = curl_exec($mr); curl_close($mr);
+    return $mr2;
+}
+ 
+ 
+function POST($host, $tsm, $data) {
+    $mr = curl_init();
+ 
+    curl_setopt_array($mr, array(
+ 
+        CURLOPT_PORT => "443",
+        CURLOPT_URL => "$host",
+        CURLOPT_RETURNTRANSFER => true,
+ 
+ 
+        CURLOPT_SSL_VERIFYPEER => false,
+ 
+        CURLOPT_CUSTOMREQUEST => "POST",
+ 
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => $tsm,
+ 
+        //CURLOPT_HEADER => true
+    ));
+    $mr2 = curl_exec($mr); curl_close($mr);
+ 
+    return $mr2;
+}
+ 
+ 
+ 
+function cc($vanban) {
+    $str = strlen($vanban);
+    for ($i = 0; $i <= $str; $i++) {
+        echo $vanban[$i]; usleep(1000);
+    }
+    return 1;
+}
+ 
+ 
+ 
+ 
+
+$a1 = "Host: gateway.golike.net";
+$a2 = "user-agent: User Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36";
+ 
+$a3="authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9nYXRld2F5LmdvbGlrZS5uZXRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTI2ODM2NjMsImV4cCI6MTc0NDIxOTY2MywibmJmIjoxNzEyNjgzNjYzLCJqdGkiOiJNQjZUcnA4YjZMWXJHTlhiIiwic3ViIjoyNTI3NjQ5LCJwcnYiOiJiOTEyNzk5NzhmMTFhYTdiYzU2NzA0ODdmZmYwMWUyMjgyNTNmZTQ4In0.-0exk2yXYgULWBnNjgQZyJOouAP5wilZo_bqj_xK8nc      ";
+ 
+$a4 = "origin: https://app.golike.net";
  
 $a5 = "t: VFZSWk5FOVVXVFJPVkdzd1RsRTlQUT09";
  
